0b43b47b3bf395ce6100d44b641217222dc32c45
victxrmarin <treneone@gmail.com>
2023-12-13 14:56:42 -0300

    Commit

--- scholar.py
+++ scholar.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt;
+
 number = int(input('Enter the student quantity: '));
 students = [];
 genders = [];
@@ -8,7 +10,7 @@ for i in range(number):
     grades = []
     
     for n in range(4):
-        grade = float(input(f'Enter {n}° grade: '));
+        grade = float(input(f'Enter {n + 1}° grade: '));
         grades.append(grade)
     
     students.append({"Name": name, "Grades": grades, "Gender": gender })
@@ -26,7 +28,24 @@ for student in students:
     avgs.append(avg);
     avg_gender[gender].append(avg);
     
-    print(f"Student's number: {i+1}");
-    print(f'Name: {name}');
-    print(f'Gender: {gender}');
+    plt.figure(figsize=(8,4));
+    plt.bar(range(number), avgs, tick_label=[student["Name"] for student in students]);
+    plt.xlabel("Students");
+    plt.ylabel("Average");
+    plt.title("Students Averages");
+    plt.savefig("students_avg.png");
+    plt.show();
+    
+    genders = list(avg_gender.keys());
+    avg_per_gender = [sum(avg_gender[gender]) / len(avg_gender[gender]) for gender in genders];
+    
+    plt.figure(figsize=(4, 4));
+    plt.bar(genders, avg_per_gender);
+    plt.xlabel('Gender');
+    plt.ylabel('Average grades');
+    plt.title("Gender Averages");
+    plt.savefig("genders_avg.png");
+    plt.show();
+    
+    
     
